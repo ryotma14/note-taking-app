ada011211798a2ca9b931b3c1ac45583d2edeabe
Ryota <ryota.masuzawa@datazoo.com>
2025-06-09 14:18:44 +0900

    Add build for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,16 +8,35 @@ import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
+import path from "path";
+
 dotenv.config();
 
 const app = express(); //Expressアプリ作成
 const PORT = process.env.PORT || 5001; //ポート番号決定
 
+const __dirname = path.resolve();
+
 // middleware
-app.use(cors({ origin: "http://localhost:5173" })); //CORS 最初に書く
+if (process.env.NODE_ENV !== "production") {
+  // 開発環境でだけCORSを許可（Reactのローカル開発環境用）
+  app.use(cors({ origin: "http://localhost:5173" })); //CORS 最初のlineに書く
+}
+
 app.use(express.json()); //JSONのリクエストボディをパース
 app.use(rateLimiter); //回数制限
 app.use("/api/notes", noteRoutes); //各エンドポイントルーティングの定義
+
+/*ViteやReactでnpm run buildした後に作られる dist フォルダ（HTML, CSS, JS が含まれる）
+をExpress サーバーが / 経由でクライアント（ブラウザ）に配信できるようにする設定*/
+if (process.env.NODE_ENV === "production") {
+  // frontendのビルドファイルを公開フォルダとして扱う
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  // React Router対応：どのパスでも index.html を返す（SPAのため）
+  app.get("*", (req, res) => {
+    res.sendFile(path.join((__dirname, "../frontend", "dist", "index.html")));
+  });
+}
 
 // connect DB → 成功したら起動
 connectDB().then(() => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5001/api" });
+// There is no localhost in PROD prpduction so it should be dynamic
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+//import.meta.env は Vite 独自の仕組み
+const api = axios.create({ baseURL: BASE_URL });
 
 export default api;
